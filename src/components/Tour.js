ea51eeb652c1a6f22b1d4e22ebf0e15966463e29
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2025-01-02 20:16:51 +0530

    Update Tour.js

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -13,14 +13,18 @@ const Tour = ({ tour, onDeleteTour }) => {
       <h2>{tour.name}</h2>
       <p id={`tour-item-para-${tour.id}`}>
         {showMore ? tour.info : `${tour.info.substring(0, 200)}...`}
-        <button onClick={toggleShowMore}>
+        <button
+          id={`see-more-${tour.id}`}  
+          onClick={toggleShowMore}
+        >
           {showMore ? "See less" : "Show more"}
         </button>
       </p>
       <p className="price">${tour.price}</p>
-      <button 
-        id={`delete-btn-${tour.id}`} 
-        onClick={() => onDeleteTour(tour.id)}>
+      <button
+        id={`delete-btn-${tour.id}`}
+        onClick={() => onDeleteTour(tour.id)}
+      >
         Delete
       </button>
     </div>
