2e14d97a67d8e03afe6a50d9fa42776f0494525b
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2025-01-02 19:37:03 +0530

    Update Tour.js

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,1 +1,27 @@
+import React, { useState } from "react";
+
+const Tour = ({ tour, onDeleteTour }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
+
+  return (
+    <div className="tour" id={tour.id}>
+      <img src={tour.image} alt={tour.name} />
+      <h2>{tour.name}</h2>
+      <p>
+        {showMore ? tour.info : `${tour.info.substring(0, 200)}...`}
+        <button onClick={toggleShowMore}>
+          {showMore ? "See less" : "Show more"}
+        </button>
+      </p>
+      <p className="price">${tour.price}</p>
+      <button onClick={() => onDeleteTour(tour.id)}>Delete</button>
+    </div>
+  );
+};
+
+export default Tour;
 
