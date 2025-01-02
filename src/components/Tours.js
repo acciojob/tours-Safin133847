65c07c7069592ec54018cd45ec34eff077be0f34
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2025-01-02 19:39:31 +0530

    Create Tours.js

---
+++ src/components/Tours.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Tour from "./Tour";
+
+const Tours = ({ tours, onDeleteTour }) => {
+  return (
+    <div id="tours-list">
+      {tours.map((tour) => (
+        <Tour key={tour.id} tour={tour} onDeleteTour={onDeleteTour} />
+      ))}
+    </div>
+  );
+};
+
+export default Tours;
